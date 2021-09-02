f67c5e80bcf1aae859f610b9e90a600c6417656c
justin-by <flaminlinkvp@gmail.com>
2021-09-02 13:15:11 -0700

    refactored portfolio website / no react simplified

--- src/components/Footer/Footer.js
+++
@@ -1,9 +0,0 @@
-import './Footer.css'
-
-const Footer = () => (
-  <footer className='footer'>
-    <span className='link footer__link'>Thank you for reading</span>
-  </footer>
-)
-
-export default Footer
